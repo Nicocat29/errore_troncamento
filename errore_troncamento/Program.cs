fb4a2da810758e8ff7cd4837a80813e483aa5109
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-09 09:10:24 +0100

    Add project files.

---
+++ errore_troncamento/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace errore_troncamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        double troncamento = 0.000000095;
+        double ore = 100;
+        double spazio = 0;
+        double metri = 1676;
+        double errore = 0;
+
+        errore = troncamento * 100 * 60 * 60 * 10;
+        spazio = metri * ore;
+
+        errore = troncamento * 100 * 60 * 60 * 10;
+        Console.WriteLine($"{errore} è il tempo errato");
+        Console.WriteLine(" ");
+
+        Console.WriteLine("calcolo dello spazio:");
+        Console.WriteLine(" ");
+        spazio = metri * ore;
+        Console.WriteLine($"{spazio} è lo spazio");
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
